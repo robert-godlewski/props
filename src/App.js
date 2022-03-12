7d2e31e075d4b6b98dba16df6702ebfe75f087d1
Robert Godlewski <robtechg5@gmail.com>
2022-03-11 19:30:55 -0800

    Added PersonCard to the React app.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+import PersonCard from './components/PersonCard';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <PersonCard 
+        first_name={"Jane"} 
+        last_name={"Doe"} 
+        age={45}
+        hair_color={"Black"} />
+      <PersonCard 
+        first_name={"John"} 
+        last_name={"Smith"} 
+        age={88} 
+        hair_color={"Brown"} />
+      <PersonCard 
+        first_name={"Millard"} 
+        last_name={"Fillmore"} 
+        age={50} 
+        hair_color={"Brown"} />
+      <PersonCard 
+        first_name={"Maria"} 
+        last_name={"Smith"} 
+        age={62} 
+        hair_color={"Brown"} />
     </div>
   );
 }
